e421fa0576175aa7cace4f2cf6794a5edd74910f
hh848 <53187679+hh848@users.noreply.github.com>
2025-04-29 15:29:08 +0800

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,7 @@
 </template>
 <script setup lang="ts">
 import Header from '@/components/Header/Header.vue'
-import Footer from '@/components/Footer/Footer.vue'
+// import Footer from '@/components/Footer/Footer.vue' 
 </script>
 
 <style scoped></style>
